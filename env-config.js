72750e97c87fdc6e08f47c1e06c7cded642909b8
Michael Immovilli <michael@yobetit.com>
2019-07-29 19:02:50 +0200

    Set env config, auth js and auth with heroku

--- env-config.js
+++ env-config.js
@@ -1,7 +1,9 @@
-const prod = process.env.NODE_ENV === 'production';
+const prod = process.env.NODE_ENV === "production";
 
 module.exports = {
-  'process.env.BASE_URL': prod ? 'https://filipjerga.herokuapp.com' : 'http://localhost:3000',
-  'process.env.NAMESPACE': 'https://filipjerga.herokuapp.com',
-  'process.env.CLIENT_ID': 'NfvS9nw81ItncHJKPHCaAvwD9ChNWYn3'
-}
+  "process.env.BASE_URL": prod
+    ? "https://portfolio-setup-michael.herokuapp.com"
+    : "http://localhost:3000",
+  "process.env.NAMESPACE": "https://portfolio-setup-michael.herokuapp.com",
+  "process.env.CLIENT_ID": "BcsMUAywbOFA6nP8TK2PVihD20WuMd62"
+};
